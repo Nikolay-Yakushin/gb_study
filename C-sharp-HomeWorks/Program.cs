e98044f863fa4b46905908ed235bcb3580929128
Nikolay_Yakushin <ny.phkp@gmail.com>
2022-03-07 23:56:04 +0300

    add different output

--- C-sharp-HomeWorks/Program.cs
+++ C-sharp-HomeWorks/Program.cs
@@ -3,17 +3,24 @@
 // Задача 1
 
 Console.WriteLine("Введите первое число");
-int numb1 = Convert.ToInt32 (Console.ReadLine());
+int numb1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число");
-int numb2 = Convert.ToInt32 (Console.ReadLine());
+int numb2 = Convert.ToInt32(Console.ReadLine());
 
 if(numb2 > numb1)
-
-    Console.Write("Большее число " + numb2); 
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(numb2);
+    Console.Write("Меньшее число ");
+    Console.WriteLine(numb1);
+    
+}
+    
 
 else
 
-Console.Write("Большее число " + numb1);
+Console.Write("Большее число " + numb1 + " " + "Меньшее число " + numb2);
+
 
 
