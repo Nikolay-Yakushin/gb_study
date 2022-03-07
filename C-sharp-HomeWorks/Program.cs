4ae2ee218e0d90c1e88de6477fbd6789061893eb
Nikolay_Yakushin <ny.phkp@gmail.com>
2022-03-07 22:51:19 +0300

    1 exircise start

---
+++ C-sharp-HomeWorks/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Вводи число");
+int numb1 = 13;
+int numb2 = 7;
+if(numb2 > numb1)
+
+    System.Console.Write("Большее число " + numb2); 
+
+else
+System.Console.Write("Большее число " + numb1);
+
+
+
+
