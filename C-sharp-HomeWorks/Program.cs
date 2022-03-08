62ec24d57c174a5113d9a36791168524bc5cdb0f
Nikolay_Yakushin <ny.phkp@gmail.com>
2022-03-08 23:02:59 +0300

    2 exircise done

--- C-sharp-HomeWorks/Program.cs
+++ C-sharp-HomeWorks/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 // Задача 1
-
+/*
 Console.WriteLine("Введите первое число");
 int numb1 = Convert.ToInt32(Console.ReadLine());
 
@@ -17,10 +17,51 @@ if(numb2 > numb1)
     
 }
     
-
 else
 
 Console.Write("Большее число " + numb1 + " " + "Меньшее число " + numb2);
+*/
+
+
+// задача 2
+
+Console.WriteLine("Введите первое целое число из трёх");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе целое число из трёх");
+    int numb2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите третье целое число из трёх");
+        int numb3 = Convert.ToInt32(Console.ReadLine());
+
+    
+if(numb1 > numb2 && numb1 > numb3)
+    {
+        Console.WriteLine("max = " + numb1);
+    }
+else
+    {
+        if(numb2 > numb3)
+        {
+            Console.WriteLine("MaX = " + numb2);
+        }
+        else
+        {
+            Console.WriteLine("MAX = " + numb3);
+        }    
+    }
+
+
+
+
+
+   
+
+
+
+    
+
+
+    
+
 
 
 
